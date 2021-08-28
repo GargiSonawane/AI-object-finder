6516576b63795b004cfbdc86b58e63e3d34f1334
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2021-08-28 15:22:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 status = "";
+object = "";
+synth = "";
+utterThis = "";
 function preload(){
 }
 
@@ -12,14 +15,47 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    if(status != ""){
+         for (i = 0; i < objects.length; i++){ 
+            object = document.getElementById("object_input").value;
+            document.getElementById("object_found").innerHTML = "Detecting object";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent +  "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("confidence").innerHTML = percent;
+        }
+    }
 }
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "status: detecting objects";
+
+    if(object == objects[i]){
+        video.stop();
+        objectDetector.detect(gotResult);
+        document.getElementById("object_found").innerHTML = "object mentioned found";
+        synth = window.speechSynthesis;
+        utterThis = new SpeechSynthesisUtterance("object mentioned found");
+        speak(utterThis);
+    }
+    else{
+        document.getElementById("object_found").innerHTML = "object mentioned not found";
+    }
 }
 
 function modelLoaded(){
     console.log("model loaded");
     status = true;
+}
+function gotResult(error, result){
+    if(error){
+        console.log(error);
+    }
+    console.log(result);
+    objects = result;
 }
